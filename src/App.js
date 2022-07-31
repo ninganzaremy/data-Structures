c557a5ad87c11fb0afa030e5287a3ad3ae54462f
Ningaanza Remy <remimaradona@gmail.com>
2022-07-30 23:39:46 -0700

    displaying coding-challenges output into a react app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import NonConstructibleChange from "./Arrays/NonConstructibleChange";
-import SmallestDifference from "./Arrays/SmallestDifference";
-import SortedSquaredArray from "./Arrays/SortedSquaredArray";
-import ThreeNumberSum from "./Arrays/ThreeNumberSum";
-import TournamentWinner from "./Arrays/TournamentWinner";
+import NonConstructibleChange from "./arrays/NonConstructibleChange";
+import SmallestDifference from "./arrays/SmallestDifference";
+import SortedSquaredArray from "./arrays/SortedSquaredArray";
+import ThreeNumberSum from "./arrays/ThreeNumberSum";
+import TournamentWinner from "./arrays/TournamentWinner";
 
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<NonConstructibleChange />
-			<SmallestDifference />
-			<SortedSquaredArray />
-			<ThreeNumberSum />
-			<TournamentWinner />
+			<div className="arrays">
+				<NonConstructibleChange />
+				<SmallestDifference />
+				<SortedSquaredArray />
+				<ThreeNumberSum />
+				<TournamentWinner />
+			</div>
 		</div>
 	);
 }
